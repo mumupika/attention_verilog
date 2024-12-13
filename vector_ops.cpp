b3e0e01fe520267a3d00a913cf713b54dcf8955e
mumujun12345 <mumujun12345@sjtu.edu.cn>
2024-12-13 16:33:56 +0800

    Completed the vector add.

--- vector_ops.cpp
+++ vector_ops.cpp
@@ -52,3 +52,51 @@ vector<int> vec_mul(vector<int> &a, vector<int> &b)
     }
     return res;
 }
+
+/**
+ * @brief 长整形加法。允許接受不同長度的向量。
+ * @param a
+ * @param b a,b 为向量。
+ * @param length 需要返回的向量长度要求
+ * @return 統一返回長度為34bits的向量。
+ */
+vector<int> vec_add(vector<int> &a, vector<int> &b, int length)
+{
+    try
+    {
+        if (a.size()==0 || b.size()==0)
+            throw "empty";
+    }
+    catch(const std::exception& e)
+    {
+        if(!a.size())
+            std::cerr << "a is empty!\n";
+        else
+            std::cerr << "b is empty!\n";
+    }
+
+
+    vector<int> ans(length, 0);
+
+    int b_ind = b.size()-1;
+    int ans_ind = length-1;
+    int carry = 0;
+    for(int i = a.size()-1; i >= 0; i--)
+    {
+        int value = a[i] + b[b_ind] + carry;
+        ans[ans_ind] = value & 1;
+        carry = value >> 1;
+
+        b_ind--;
+        ans_ind--;
+    }
+    for(;b_ind >= 0; b_ind--)
+    {
+        int value = b[b_ind] + carry;
+        ans[ans_ind] = value & 1;
+        carry = value >> 1;
+        ans_ind--;
+    }
+    if(ans_ind != 0) ans[ans_ind] += carry;
+    return ans;
+}
